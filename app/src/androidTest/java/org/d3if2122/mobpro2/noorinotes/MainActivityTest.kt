81413a1fbe8f3e53601383f638ec825d6f0bf5b2
SyahrurRZN <syahrurtkj@gmail.com>
2021-12-01 22:10:31 +0700

    Fixing New Baru

--- app/src/androidTest/java/org/d3if2122/mobpro2/noorinotes/MainActivityTest.kt
+++ app/src/androidTest/java/org/d3if2122/mobpro2/noorinotes/MainActivityTest.kt
@@ -30,18 +30,13 @@ class MainActivityTest {
     @Test
     fun testInsert() {
 //        val activityScenario = ActivityScenario.launch( MainActivity::class.java)
-//
-//// Lakukan aksi menambah data baru
-        onView(withId(R.id.calendarView)).perform(PickerActions.setDate(2021, 12, 31))
-        onView(withId(R.id.fab)).perform(click())
-        onView(withId(R.id.iGambar)).perform(click())
-//        onView(withText("Gallery"))
-//            .inRoot(isDialog())
-//            .perform(click())
+
+//// Lakukan aksi melihat histori
+        onView(withId(R.id.historyrecycle)).perform(click())
 
 // Cek apakah hasil sesuai yang diharapkan onView(withText(MAHASISWA_DUMMY.nim)).check(matches(isDisplayed())) onView(withText(MAHASISWA_DUMMY.nama)).check(matches(isDisplayed()))
 
-//// Tes selesai, tutup activity nya
+// Tes selesai, tutup activity nya
 //        activityScenario.close()
     }
 
