424c6d65f9e132e47ebb30a8cd3fbe297bb18fd6
SyahrurRZN <syahrurtkj@gmail.com>
2021-12-02 11:18:50 +0700

    Fixing New Baru lagi

--- app/src/androidTest/java/org/d3if2122/mobpro2/noorinotes/MainActivityTest.kt
+++ app/src/androidTest/java/org/d3if2122/mobpro2/noorinotes/MainActivityTest.kt
@@ -1,28 +1,38 @@
-//package org.d3if2122.mobpro2.noorinotes
-//
-//
-//import androidx.test.ext.junit.runners.AndroidJUnit4 import org.junit.Test
-//import org.junit.runner.RunWith
-//import androidx.recyclerview.widget.RecyclerView
-//import androidx.test.espresso.Espresso
-//import androidx.test.espresso.action.ViewActions.click
-//import androidx.test.espresso.contrib.RecyclerViewActions
-//import androidx.test.espresso.matcher.ViewMatchers.withId
-//
-//
-//@RunWith(AndroidJUnit4::class)
-//class MainActivityTest {
-//
-//// Cek Recycleview
-//
-//    @Test
-//    fun recyleViewTest(){
-//
-//       Espresso.onView((withId(R.id.historyrecycle))).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(2, click()))
-//
-//    }
-//// Tes selesai, tutup activity nya
-////        activityScenario.close()
-//}
+package org.d3if2122.mobpro2.noorinotes
+
+
+import androidx.test.ext.junit.runners.AndroidJUnit4 import org.junit.Test
+import org.junit.runner.RunWith
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.withId
+
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+// Cek Recycleview
+
+    @Test
+    fun recyleViewTest() {
+        val activityScenario = ActivityScenario.launch(
+            MainActivity::class.java
+        )
+
+        Espresso.onView((withId(R.id.historyrecycle))).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                2,
+                click()
+            )
+        )
+
+
+// Tes selesai, tutup activity nya
+    activityScenario.close()
+}
+}
 
 
